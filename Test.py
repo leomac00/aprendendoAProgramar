c9c9c373c6876069157a2576c1447faf32a52ba2
leomac00 <67433985+leomac00@users.noreply.github.com>
2020-07-02 00:15:26 -0300

    Automate Boring Stuff With Python

---
+++ Test.py
@@ -0,0 +1,17 @@
+print(round(-12.59999))
+#########################################
+# spam = 1
+# while spam <= 10:
+#     print(spam)
+#     spam += 1
+##########################################
+# for i in range (1, 11):
+#     print(i)
+##########################################
+# spam = int(input('Type in a value: '))
+# if spam == 1:
+#     print('Hello')
+# elif spam == 2:
+#     print('Howdy')
+# else:
+#     print('Greetings')

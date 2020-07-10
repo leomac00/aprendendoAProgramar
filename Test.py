95f6b88948d8221a47dc0b1c78927154ce7afb3c
leomac00 <67433985+leomac00@users.noreply.github.com>
2020-07-10 19:01:13 -0300

    10-07-2020

--- Test.py
+++ Test.py
@@ -1,6 +1,30 @@
-def functionn():
-    pass
-print(type(functionn()))
+
+################################################
+# bacon = [3.14, 'cat', 11, 'cat', True]
+# print(bacon.index('cat'))
+################################################
+# spam = ["a","b","c","d"]
+# print(spam[int(int('3' * 2) / 11)])
+# print(spam[:2])
+#################################################
+#este programa testa possiveis erros ao acessar valores dentro de uma lista
+# import random
+# spam = 5
+# lista = ["gato","rato"]
+# try:
+#     print(lista[spam])
+# except TypeError:
+#     print("erro no tipo do valor do argumento da lista, convertendo para int")
+#     spam = int(round(spam))
+# except IndexError:
+#     spam = random.randint(0,len(lista)-1)
+#     print("erro no indice da lista, acessando o indice aleatorio no. {} da lista".format(spam))
+# finally:
+#     print(lista[spam])
+########################################
+# def functionn():
+#     pass
+# print(type(functionn()))
 ########################################
 #print(round(-12.59999))
 #########################################

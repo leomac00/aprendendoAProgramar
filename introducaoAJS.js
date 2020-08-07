8a588160c1e48d030743aaeb4fee7874d2b2bb12
leomac00 <67433985+leomac00@users.noreply.github.com>
2020-08-07 16:38:58 -0300

    Add files via upload

---
+++ introducaoAJS.js
@@ -0,0 +1,59 @@
+//Funções
+
+//----------
+
+// //Trabalhando com data
+// var d = new Date;
+// alert(d);
+// console.log(d.getMonth());
+// console.log(d.getDate());
+// console.log(d.getDay());
+// console.log(d.getHours());
+// console.log(d.getMilliseconds());
+
+//----------
+
+// //Loops
+// var countW = 0;
+// while (countW <= 3){
+//     console.log("contagem do loop 'while' : " + countW);
+//     countW ++;
+// }
+
+// for (var i = 0; i <=3; i++){
+//     console.log("contagem do loop 'for' : " + i)
+// }
+
+
+//----------
+
+// //Exemplo de laço IF e comando Prompt() : 
+// var idade = prompt("Qual sua idade?");
+// if (idade >= 18){
+//     alert("Maior de idade!");
+// }else{
+//     alert("Menor de idade!");
+// }
+
+//----------
+
+// //Dicionarios
+// var fruta = {nome: "Maçã",
+//               cor: "Vermalho"};
+// console.log(fruta.nome)              
+
+// ----------
+
+// // métodos de listas
+// var lista = ["gato", "sapato", "cadarço", "periquito", "salsicha"];
+// alert(lista.toString())
+// console.log(lista.reverse().toString())
+
+//----------
+
+// //Exemplo de alert
+// var mensagem = 'Testando mensagem em alert';
+// alert(mensagem);
+
+
+//Script desenvolvido a partir do seguinte curso: https://web.digitalinnovation.one/course/programacao-para-internet-com-javascript/learning/33e81edf-6f9b-4b97-b3c6-12d930cf0831/

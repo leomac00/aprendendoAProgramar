40ab07c1dbaf66ed90879910480d7e2a78768a89
leomac00 <67433985+leomac00@users.noreply.github.com>
2020-08-11 15:05:29 -0300

    Add files via upload

---
+++ codeWars.js
@@ -0,0 +1,38 @@
+//----------
+// function binaryArrayToNumber(arr){
+//   var result = 0;
+//   var size = arr.length
+//   var counter = 0;
+//   for (let i = arr.length-1; i >= 0; i--){
+//     result += arr[i]*(2**counter)
+//     counter++
+//   }
+//   return result
+// }
+
+// binaryArrayToNumber([0,0,0,1])
+
+//----------
+// function getCount(str) {
+//     var vowelsCount = 0;
+//     var vowels = ["a", "e", "i", "o", "u"]
+//     for (var i = 0; i <= str.length; i++){
+//       if (vowels.includes(str[i])){
+//           vowelsCount += 1;
+//       }
+//     }
+//       return vowelsCount;
+//   }
+  
+//   console.log(getCount('testando dois'))
+  //----------
+  // function descendingOrder(n){
+  //     n = n + "";
+  //     n = n.split("");
+  //     n = n.sort();
+  //     n = n.reverse();
+  //     return n;
+  //   }
+    
+  //   var list = 456123;
+  //   console.log(descendingOrder(list));
